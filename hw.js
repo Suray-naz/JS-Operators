9a65bcc983d9bb31295b76f8853806ce7202c616
Suray-naz <129758167+Suray-naz@users.noreply.github.com>
2023-05-16 16:09:21 +0200

    add

--- hw.js
+++ hw.js
@@ -1,10 +1,25 @@
-// ODEV : Girilen sayının kaç basamaklı olduğunu bulan programı while loop ile yapınız 
+// !ODEV1 : Girilen sayının kaç basamaklı olduğunu bulan programı while loop ile yapınız 
 
-let n=+prompt("Bir sayi giriniz")
-let result=0;
+// let n=+prompt("Bir sayi giriniz")
+// let result=0;
 
-while(n>0){
-    n=Math.floor(n/10);
-    result++;
+// while(n>0){
+//     n=Math.floor(n/10);
+//     result++;
   
-} console.log(result);
+// } console.log(result);
+
+
+
+// !ÖDEV : 1-10 arasında Rastgele seçilen bir sayıyı kullanıcının bulmasını isteyen program
+
+let random=Math.ceil(Math.random()*10);
+let num=+prompt("1-10 arasinda bir sayi giriniz:")
+
+if(random === num){
+    document.write(`Bravooo! Dogru tahmin: ${random} `)
+}
+
+else{
+    document.write(`Yanlış tahmin! Tekrar deneyiniz: ${random} `)
+}
